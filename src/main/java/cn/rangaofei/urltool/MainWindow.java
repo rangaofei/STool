9096f05f1479366cd4f09a063f19a2f6e764079c
saka <876038559@qq.com>
2021-12-03 15:01:15 +0800

    optimize the main window code

--- src/main/java/cn/rangaofei/urltool/MainWindow.java
+++ src/main/java/cn/rangaofei/urltool/MainWindow.java
@@ -1,33 +1,18 @@
 package cn.rangaofei.urltool;
 
-import cn.rangaofei.urltool.bar.BarCodeListener;
 import cn.rangaofei.urltool.bar.BarPanel;
-import cn.rangaofei.urltool.bar.BarcodeTool;
-import cn.rangaofei.urltool.codec.Base64Util;
-import cn.rangaofei.urltool.codec.CodecButtonListener;
 import cn.rangaofei.urltool.codec.CodecPanel;
 import cn.rangaofei.urltool.time.TimePanel;
 import cn.rangaofei.urltool.url.UrlParseListener;
 import cn.rangaofei.urltool.url.UrlToolPanel;
-import cn.rangaofei.urltool.widget.ScrollTextArea;
-import com.google.zxing.WriterException;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.components.JBTabbedPane;
-import com.intellij.util.ui.JBUI;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 public class MainWindow extends JPanel {
-    private JBTabbedPane tabBar;
-    private CodecPanel codecPanel;
-    private BarPanel barPanel;
     private UrlToolPanel urlToolPanel;
-    private TimePanel timePanel;
 
     public MainWindow(ToolWindow toolWindow) {
         initView();
@@ -35,19 +20,8 @@ public class MainWindow extends JPanel {
 
     private void initView() {
         this.setLayout(new BorderLayout());
-        initCodecPanel();
-        initBarPanel();
         initUrlToolPanel();
-        initTimePanel();
         initTab();
-    }
-
-    private void initCodecPanel() {
-        codecPanel = new CodecPanel();
-    }
-
-    private void initBarPanel() {
-        barPanel = new BarPanel();
     }
 
     private void initUrlToolPanel() {
@@ -59,15 +33,11 @@ public class MainWindow extends JPanel {
         });
     }
 
-    private void initTimePanel(){
-        timePanel = new TimePanel();
-    }
-
     private void initTab() {
-        tabBar = new JBTabbedPane();
-        tabBar.insertTab("Codec", null, codecPanel, "", 0);
-        tabBar.insertTab("BarCode", null, barPanel, "", 1);
-        tabBar.insertTab("Time",null,timePanel,"",2);
+        JBTabbedPane tabBar = new JBTabbedPane();
+        tabBar.insertTab("Codec", null, new CodecPanel(), "", 0);
+        tabBar.insertTab("BarCode", null, new BarPanel(), "", 1);
+        tabBar.insertTab("Time", null, new TimePanel(), "", 2);
 //        tabBar.insertTab("UrlTool", null, urlToolPanel, "", 2);
         this.add(tabBar, BorderLayout.CENTER);
     }
